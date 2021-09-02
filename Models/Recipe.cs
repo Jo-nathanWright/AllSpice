38f2f90f82ef576dec30333c41565bc8764228f3
Jonathan Wright <j.wright72127@gmail.com>
2021-09-02 13:57:44 -0600

    Made Recipe Model

---
+++ Models/Recipe.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AllSpice.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CookTime { get; set; }
+        public int PrepTime { get; set; }
+        public string CreatorId { get; set; }
+
+
+  }
+}
